cad3d25417279bb0ad39b357428e8a09ebdc242d
Martin Tonhauzer <unknow112@gmail.com>
2024-01-29 00:26:24 +0100

    proc stage 8 optimizations

--- processing/8_paper_ids_is_referenced.py
+++ processing/8_paper_ids_is_referenced.py
@@ -1,6 +1,8 @@
 import json
 import os
 from collections import defaultdict
+import bz2
+from multiprocessing import Pool
 
 import click
 from src.oneliner_utils import join_path, read_list, write_list
@@ -19,10 +21,9 @@ def get_doc_ids(lang: str, fos: str):
     paper_references_path = join_path(raw_data_path, "paper_references.jsonl")
     doc_ids_path = join_path(dataset_path, "doc_ids.txt")
     final_doc_ids_path = join_path(dataset_path, "final_doc_ids.txt")
-    doc_ids = set(read_list(doc_ids_path))
+    doc_ids_with_timestamps = dict.fromkeys(read_list(doc_ids_path), None)
 
-    paper_date_dict = {}
-    with open(papers_path, "r") as papers_f:
+    with bz2.open(papers_path, "r") as papers_f:
         for line in tqdm(
             papers_f,
             mininterval=1.0,
@@ -31,13 +32,13 @@ def get_doc_ids(lang: str, fos: str):
         ):
             paper = json.loads(line)
             paper_id = paper["id"]
-            if paper_id in doc_ids:
-                paper_date_dict[paper_id] = paper["timestamp"]
+            if paper_id in doc_ids_with_timestamps:
+                doc_ids_with_timestamps[paper_id] = paper["timestamp"]
 
-    paper_references_dict = defaultdict(list)
+    paper_references_dict = {}
     is_referenced_ids = set()
 
-    with open(paper_references_path, "r") as f_in:
+    with bz2.open(paper_references_path, "r") as f_in:
         for line in tqdm(
             f_in,
             mininterval=1.0,
@@ -47,19 +48,19 @@ def get_doc_ids(lang: str, fos: str):
             paper_references = json.loads(line)
             doc_id = paper_references["doc_id"]
 
-            if doc_id in doc_ids:
-                date = paper_date_dict[doc_id]
+            if doc_id in doc_ids_with_timestamps:
+                date = doc_ids_with_timestamps[doc_id]
                 rel_doc_ids = paper_references["rel_doc_ids"]
 
                 # Keep only references in current fos and pointing backword
-                rel_doc_ids = [
+                rel_doc_ids = set(
                     x
                     for x in rel_doc_ids
-                    if x in doc_ids and paper_date_dict[x] < date
-                ]
+                    if x in doc_ids_with_timestamps and doc_ids_with_timestamps[x] < date
+                )
 
                 paper_references_dict[doc_id] = rel_doc_ids
-                is_referenced_ids |= set(rel_doc_ids)
+                is_referenced_ids |= rel_doc_ids
 
     while len(is_referenced_ids) > 5_000_000:
         paper_references_dict = {
